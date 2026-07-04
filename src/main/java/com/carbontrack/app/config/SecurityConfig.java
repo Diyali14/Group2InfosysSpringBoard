47abc821b4e2e4e64cdd87cc1be12586928c6df3
Diyali14 <diyali14082004@gmailcom>
2026-07-04 23:23:41 +0530

    Implement JWT authentication with Google OAuth2 integration

--- src/main/java/com/carbontrack/app/config/SecurityConfig.java
+++ src/main/java/com/carbontrack/app/config/SecurityConfig.java
@@ -13,23 +13,26 @@ import com.carbontrack.app.security.CustomUserDetailsService;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import com.carbontrack.app.oauth.OAuth2LoginSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
+    private final OAuth2LoginSuccessHandler successHandler;
     private final CustomUserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
     public SecurityConfig(
             JwtAuthenticationFilter jwtAuthenticationFilter,
             CustomUserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
+            PasswordEncoder passwordEncoder,
+            OAuth2LoginSuccessHandler successHandler) {
 
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
+        this.successHandler = successHandler;
     }
 
     @Bean
@@ -44,7 +47,9 @@ public class SecurityConfig {
                 ).sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).httpBasic(httpBasic -> httpBasic.disable())
-                .formLogin(formLogin -> formLogin.disable()).authenticationProvider(authenticationProvider()).addFilterBefore(
+                .formLogin(formLogin -> formLogin.disable()).oauth2Login(oauth ->
+                        oauth.successHandler(successHandler)
+                ).authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtAuthenticationFilter,
                         UsernamePasswordAuthenticationFilter.class
                 )
